9ab0886dc030b8b1ddeb778ed797c9c5e19f0d44
usename02 <1275730212.com>
2018-12-10 17:59:10 +0800

    功能完善

--- h_ui_tp5/application/index/model/Member.php
+++ h_ui_tp5/application/index/model/Member.php
@@ -20,6 +20,8 @@ class Member extends Model
     //设置当前表默认日期时间显示格式
     protected $dateFormat = 'Y/m/d';
 
+//定义表中的 服务号OPENID
+    protected $OPENID_FFH = 'OPENID_FFH';
     //定义表中的删除时间字段,来记录删除时间
     protected $deleteTime = 'delete_time';
 
@@ -33,8 +35,7 @@ class Member extends Model
     protected $type = [
         'hiredate'=>'timestamp'
     ];
-//定义表中的 服务号OPENID
-    protected $OPENID_FFH = 'OPENID_FFH';
+
     // 定义自动完成的属性
     protected $insert = ['status' => 1];
 }
